cf20f3cb5db0e24a186f057e89914dda9f25909e
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-10-04 18:21:05 +0100

    push SFML library + Basic Code
    Code just opens to a black window

--- sourceCode/main.cpp
+++ sourceCode/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <SFML/Graphics.hpp>
 
 using namespace sf;
 using namespace std;
@@ -9,12 +10,35 @@ void Load() {
 
 void Update(RenderWindow& window) {
 
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    // check and consume events
+    Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+    // Quit Via ESC Key
+    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+        window.close();
+    }
 }
 
 void Render(RenderWindow& window) {
-
+    // Draw Everything
 }
 
 int main() {
-	
+    RenderWindow window(VideoMode(800, 600), "Ball!");
+    Load();
+    while (window.isOpen()) {
+        window.clear();
+        Update(window);
+        Render(window);
+        window.display();
+    }
+    return 0;
 }

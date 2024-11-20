ce46c2f85e401bf2f28e1597f56780180bc8e931
D1A7N4 <daniel.p.seavers@gmail.com>
2024-11-20 19:28:23 +0000

    added icon to menu and attempted to reposition text

--- lab_7_platformer/scenes/scene_menu.cpp
+++ lab_7_platformer/scenes/scene_menu.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Window/Keyboard.hpp>
 #include "../components/cmp_sprite.h"
 #include <iostream>
+#include "engine.h"
 
 using namespace std;
 using namespace sf;
@@ -11,17 +12,22 @@ using namespace sf;
 void MenuScene::Load() {
   cout << "Menu Load \n";
   {
-    Texture coconut;
+    //add text to screen
     auto txt = makeEntity();
     auto t = txt->addComponent<TextComponent>(
         "Circle: The Last Edgebender\nPress Space to Start");
-    auto shape = makeEntity();
-        if (!coconut.loadFromFile("res/images/coconutj.jpg")) {
+    //NOT CURRENTLY WORKING
+    txt->setPosition(Vector2f(Engine::getWindowSize().x * .45f, .0f)); 
+    //add icon to screen
+    Texture iconfile;
+    auto iconentity = makeEntity();
+        if (!iconfile.loadFromFile("res/images/icon.png")) {
     cerr << "Failed to load spritesheet!" << std::endl;
   }
-    auto s = shape->addComponent<SpriteComponent>();
-    auto ptr = make_shared<Texture>(coconut);
-    s->setTexure(ptr);
+    auto icon = iconentity->addComponent<SpriteComponent>();
+    auto ptr = make_shared<Texture>(iconfile);
+    icon->setTexure(ptr);
+    iconentity->setPosition(Vector2f(Engine::getWindowSize().x * .25f, Engine::getWindowSize().y * .15f));
   }
   setLoaded(true);
 }

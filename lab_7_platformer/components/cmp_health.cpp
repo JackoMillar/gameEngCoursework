352756c4541a163308d34080896ca792cea557be
D1A7N4 <daniel.p.seavers@gmail.com>
2024-11-27 00:31:52 +0000

    added hp component to player

---
+++ lab_7_platformer/components/cmp_health.cpp
@@ -0,0 +1,30 @@
+#include "cmp_health.h"
+#include <engine.h>
+#include <system_renderer.h>
+#include <system_resources.h>
+
+int maxHP;
+int hitPoints;
+
+void HealthPointComponent::hurt(int damage){
+    hitPoints -= damage;
+    if(hitPoints < 0){
+        //INSERT GAME OVER CODE HERE!!!
+    }
+}
+
+void HealthPointComponent::heal(int healing){
+    hitPoints += healing;
+    if(hitPoints > maxHP){
+        hitPoints = maxHP;
+    }
+}
+
+void HealthPointComponent::update(double dt){}
+
+void HealthPointComponent::render(){}
+
+HealthPointComponent::HealthPointComponent(Entity *const p, int maxHp) : Component(p), maxHP(maxHp)
+{
+    hitPoints = maxHP;
+}

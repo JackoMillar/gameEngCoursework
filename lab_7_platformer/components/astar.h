9a880f8b3714327e98e33cbda32704eaa923f121
Maja <115842945+maja4h@users.noreply.github.com>
2024-12-11 18:08:28 +0000

    Deleting algorithm files

--- lab_7_platformer/components/astar.h
+++
@@ -1,9 +0,0 @@
-//astar.h
-//pathfinding decision making behaviour
-#pragma once
-
-#include <engine.h>
-#include <vector>
-
-//vector with starting position and target position
-std::vector<sf::Vector2i> pathFind(sf::Vector2i start, sf::Vector2i finish);
